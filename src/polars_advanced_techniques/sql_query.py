539531fb5beb0033b711ba2efa2a5994a147da36
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-21 11:15:47 -0400

    Create sql_query.py

---
+++ src/polars_advanced_techniques/sql_query.py
@@ -0,0 +1,22 @@
+# Uses Polars SQL to perform joins and conditional logic on DataFrames.
+
+import polars as pl
+
+df1 = pl.DataFrame({"id": [1, 2, 3], "value": [15, 25, 35]})
+df2 = pl.DataFrame({"id": [1, 2, 4], "category": ["X1", "Y2", "Z3"]})
+
+result = pl.sql("""
+    SELECT 
+        a.id,
+        a.value,
+        b.category,
+        CASE 
+            WHEN a.value > 20 THEN 'High'
+            ELSE 'Low'
+        END as segment
+    FROM df1 a
+    LEFT JOIN df2 b ON a.id = b.id
+    WHERE a.value > 10
+""")
+
+print(result)

db4f18acee33d9f976247d6f24378b182cf6bd00
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-21 11:15:07 -0400

    Create map_batches.py

---
+++ src/polars_advanced_techniques/map_batches.py
@@ -0,0 +1,17 @@
+# Applies a custom function using map_batches in Polars to calculate percentiles on list columns.
+
+import numpy as np
+import polars as pl
+
+df = pl.DataFrame({
+    "values": [[2, 4, 6], [3, 5, 7], [1, 8, 9]]
+})
+
+def calculate_percentile(series):
+    return pl.Series([np.percentile(x, 80) for x in series])
+
+result = df.with_columns(
+    pl.col("values").map_batches(calculate_percentile).alias("p80")
+)
+
+print(result)
